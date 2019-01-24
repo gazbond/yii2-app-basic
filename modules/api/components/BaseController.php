20be558c6e4a3208b376c71fc7971d2c2ebbf910
Gazbond <dev@gazbond.co.uk>
2019-01-24 13:45:21 +0000

    Updated to use elastic model as well as active record including default actions.

--- modules/api/components/BaseController.php
+++ modules/api/components/BaseController.php
@@ -2,9 +2,22 @@
 
 use yii\rest\ActiveController;
 use sizeg\jwt\JwtHttpBearerAuth;
+use yii\web\NotFoundHttpException;
+use Yii;
+use app\components\BaseElasticRecord;
 
+/**
+ * Class BaseController.
+ *
+ * @package app\modules\api\components
+ */
 class BaseController extends ActiveController
 {
+    /**
+     * @var string
+     */
+    public $elasticClass = null;
+
     /**
      * @inheritdoc
      */
@@ -16,4 +29,45 @@ class BaseController extends ActiveController
         ];
         return $behaviors;
     }
-} 
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['index']);
+        unset($actions['view']);
+        return $actions;
+    }
+
+    /**
+     * @return array
+     */
+    public function actionIndex()
+    {
+        /** @var BaseElasticRecord $model */
+        $model = Yii::createObject($this->elasticClass, [
+            'scenario' => 'search'
+        ]);
+        $params = Yii::$app->request->queryParams;
+        $model->setAttributes($params);
+        return $model->search();
+    }
+
+    /**
+     * @param $id
+     * @return array|mixed|null|\yii\elasticsearch\ActiveRecord|static
+     * @throws NotFoundHttpException
+     */
+    public function actionView($id)
+    {
+        /** @var BaseElasticRecord $modelClass */
+        $modelClass = $this->elasticClass;
+        $result = $modelClass::findOne($id);
+        if($result === null) {
+            throw new NotFoundHttpException();
+        }
+        return $result;
+    }
+}
