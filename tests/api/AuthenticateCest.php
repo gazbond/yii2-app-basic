cc873f44b119a482edbd2fdb35bd72eb3f06392c
Gazbond <dev@gazbond.co.uk>
2019-01-24 13:39:23 +0000

    Updated to check auth token.

--- tests/api/AuthenticateCest.php
+++ tests/api/AuthenticateCest.php
@@ -1,27 +1,22 @@
 <?php
 
-use yii\helpers\Url;
-
 class AuthenticateCest
 {
-    private $token;
-
-    public function _before(ApiTester $I)
-    {
-
-    }
-
-    // tests
     public function checkMeIsAuthenticated(ApiTester $I)
     {
-        $I->amGoingTo('POST login credentials');
-        $I->sendPOST(Url::toRoute('/api/security/login'), [
+        $I->amGoingTo('post login credentials');
+        $I->sendPOST('/api/security/login', [
             'login' => 'root',
             'password' => 'password'
         ]);
         $I->seeHttpHeader('Authorization');
-        $this->token = $I->grabHttpHeader('Authorization');
+        $token = $I->grabHttpHeader('Authorization');
         $I->amGoingTo('use token to access api');
-        $I->sendGET(Url::toRoute('/api/users/me'));
+        $I->amBearerAuthenticated($token);
+        $I->sendGET('/api/users/me');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContainsJson([
+            'username' => 'root'
+        ]);
     }
 }
