79e76dacc05a65999aa18ff4a51d467ec79ebc7d
Gazbond <dev@gazbond.co.uk>
2019-02-01 00:19:49 +0000

    Added interactive option. Can run file with ./docker.sh (wait for databases to be ready).

--- docker.sh
+++ docker.sh
@@ -2,24 +2,24 @@
 cd /app
 
 # Migrations
-yii migrate --migrationPath=@vendor/dektrium/yii2-user/migrations
-yii migrate --migrationPath=@yii/rbac/migrations
+yii migrate --migrationPath=@vendor/dektrium/yii2-user/migrations --interactive=0
+yii migrate --migrationPath=@yii/rbac/migrations --interactive=0
 
 # ElasticSearch
 yii mapping
 
 # Fixtures
-yii fixture User
+yii fixture User --interactive=0
 
 # Test Directory
 cd /app/tests/bin
 
 # Migrations
-./yii migrate --migrationPath=@vendor/dektrium/yii2-user/migrations
-./yii migrate --migrationPath=@yii/rbac/migrations
+./yii migrate --migrationPath=@vendor/dektrium/yii2-user/migrations --interactive=0
+./yii migrate --migrationPath=@yii/rbac/migrations --interactive=0
 
 # ElasticSearch
 ./yii mapping
 
 # Fixtures
-./yii fixture User
+./yii fixture User --interactive=0
