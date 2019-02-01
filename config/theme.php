cc00af8e8e6090858360978188d02883333ab92c
Gazbond <dev@gazbond.co.uk>
2019-02-01 17:34:20 +0000

    Added theme views for user module. No changes yet.

--- config/theme.php
+++ config/theme.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
-    'basePath' => '',
 // Override module view files
-//    'pathMap' => [
-//        '@vendor/dektrium/yii2-user/views' => '@app/theme/user/views',
+    'pathMap' => [
+        '@vendor/dektrium/yii2-user/views/mail/' => '@app/themes/user/mail/',
+        '@vendor/dektrium/yii2-user/views/profile/' => '@app/themes/user/profile/',
+        '@vendor/dektrium/yii2-user/views/recovery/' => '@app/themes/user/recovery/',
+        '@vendor/dektrium/yii2-user/views/registration/' => '@app/themes/user/registration/',
+        '@vendor/dektrium/yii2-user/views/security/' => '@app/themes/user/security/',
 //        '@vendor/dektrium/yii2-rbac/views' => '@app/theme/rbac/views',
-//        '@vendor/yii2mod/yii2-settings/views/' => '@app/theme/settings/views'
-//    ]
+    ]
 ];
